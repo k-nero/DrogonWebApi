c819128a890907e561e6f6c765f7f59ddc0f0bf2
Nero <67089844+k-nero@users.noreply.github.com>
2024-02-15 12:41:03 +0700

    patch macro

--- Application/ApplicationApi.h
+++ Application/ApplicationApi.h
@@ -14,11 +14,11 @@
 #define LTE(x) #x " <= '" + x + "'"
 #define LIKE(x) #x " LIKE '%" + x + "%'"
 #define NOT_LIKE(x) #x " NOT LIKE '%" + x + "%'"
-#define IN(x) #x " IN (" + x + ")"
+#define _IN(x) #x " IN (" + x + ")"
 #define NOT_IN(x) #x " NOT IN (" + x + ")"
 #define IS_NULL(x) #x " IS NULL"
 #define IS_NOT_NULL(x) #x " IS NOT NULL"
 #define AND(x, y) x _AND y
 #define OR(x, y) x _OR y
-#define _AND " AND "
-#define _OR " OR "
+constexpr auto _AND = " AND ";
+constexpr auto _OR = " OR ";
