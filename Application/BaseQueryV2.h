d28e6ed22b6fc8b8340178548fd79f3b58c4f2a5
Nero <67089844+k-nero@users.noreply.github.com>
2024-03-01 06:33:30 +0700

    draft

--- Application/BaseQueryV2.h
+++ Application/BaseQueryV2.h
@@ -37,12 +37,9 @@ public:
 			std::string table_name = std::string(typeid(K).name()).substr(table_name.find_last_of(' ') + 1);
 			std::string query = "SELECT ";
 
-			boost::mp11::mp_for_each< boost::describe::describe_members<K, boost::describe::mod_any_access | boost::describe::mod_inherited>>([&](auto D)
-			{
-				std::string name = D.name;
-				query += table_name + "." + name + " as " + name;
-				query += ",";
-			});
+			std::vector<std::string> tables = { table_name };
+			tables.insert(tables.end(), includes.begin(), includes.end());
+			std::reverse(tables.begin(), tables.end());
 
 
 			SACommand cmd(con.get(), _TSA(query.c_str()));
@@ -64,4 +61,74 @@ public:
 		}
 		return nullptr;
 	}
+
+private:
+	template<typename Type, std::enable_if_t<std::is_class_v<Type>, bool> = true>
+	int field_count()
+	{
+		int count = 0;
+		Type t{};
+		boost::mp11::mp_for_each< boost::describe::describe_members<Type, boost::describe::mod_any_access | boost::describe::mod_inherited>>([&](auto D)
+		{
+			auto value = t.*(D).pointer;
+			if (is_primitive_type(value))
+			{
+				count++;
+			}
+		});
+	}
+
+	template<typename Type, std::enable_if_t<std::is_void_v<Type>, bool> = true>
+	auto field_count()
+	{
+		return 0;
+	}
+
+
+
+	template <typename Type, std::enable_if_t<std::is_class_v<Type>, bool> = true>
+	std::string table_alias_builder( std::vector<std::string> table_list )
+	{
+		std::string alias = "";
+		Type t{};
+		boost::mp11::mp_for_each< boost::describe::describe_members<Type, boost::describe::mod_any_access | boost::describe::mod_inherited>>([&](auto D)
+		{
+			auto value = t.*(D).pointer;
+			if (is_primitive_type(value))
+			{
+				alias += "[" + table_list.back() + "]" + "." + (D).name + " AS " + table_list.back() + "_" + (D).name + ", ";
+			}
+			else if (std::find(table_list.begin(), table_list.end(), D.name) != table_list.end())
+			{
+				using type = std::remove_reference_t<decltype(item.get()->*(D).pointer)>;
+				using inner_type = has_value_type_t<type>;
+
+				if (!std::is_void_v<inner_type> && !std::is_same_v<std::string, type> && !std::is_same_v<std::wstring, type>)
+				{
+					using inner_elem_type = std::remove_pointer_t<has_element_type_t<std::remove_reference_t<inner_type>>>;
+
+					if (!std::is_void_v<inner_elem_type>)
+					{
+
+					}
+				}
+				else if (is_shared_ptr_v<type>)
+				{
+					using inner_elem_type = std::remove_pointer_t<has_element_type_t<std::remove_reference_t<type>>>;
+					if (!std::is_void_v<inner_elem_type>)
+					{
+
+					}
+				}
+
+			}
+		});
+	}
+
+	template <typename Type, std::enable_if_t<std::is_void_v<Type>, bool> = true>
+	auto table_alias_builder(std::vector<std::string> table_list)
+	{
+		return 0;
+	}
+
 };
