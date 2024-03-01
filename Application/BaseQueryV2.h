06dee496343728a26d1a9c97ac51cab8178fb195
Nero <67089844+k-nero@users.noreply.github.com>
2024-03-01 12:53:07 +0700

    fast get

--- Application/BaseQueryV2.h
+++ Application/BaseQueryV2.h
@@ -21,11 +21,17 @@ template <typename T, typename Z = std::is_base_of<BaseEntity, T>::type>
 class Query
 {
 public:
-	Query() = default;
-	~Query() = default;
+	Query()
+	{
+		if (!db)
+		{
+			this->db = std::make_unique<DbContext>();
+		}
+	}
+		~Query() = default;
 
 	template<typename K = T>
-	std::shared_ptr<K> GetById(const std::string& id, std::vector<std::string> includes = {}) noexcept(false)
+	std::string FastGetById(const std::string& id, std::vector<std::string> includes = {}) noexcept(false)
 	{
 		std::shared_ptr<SAConnection> con(db->GetConnection());
 		try
@@ -34,18 +40,19 @@ public:
 			{
 				throw std::exception("Id is empty");
 			}
-			std::string table_name = std::string(typeid(K).name()).substr(table_name.find_last_of(' ') + 1);
-			std::string query = "SELECT ";
-
-			std::vector<std::string> tables = { table_name };
-			tables.insert(tables.end(), includes.begin(), includes.end());
-			std::reverse(tables.begin(), tables.end());
-
-
+			std::string table_name = std::string(typeid(K).name());
+			table_name = table_name.substr(table_name.find_last_of(' ') + 1);
+			std::string query = "SELECT *";
+			std::string alias = include_table<K>(includes);
+			std::string from = " FROM [dbo].[" + table_name + "] WHERE Id = '" + id + "' FOR JSON AUTO";
+			query += alias + from;
 			SACommand cmd(con.get(), _TSA(query.c_str()));
-			const SAString idStr(id.c_str());
-			cmd.Param(_TSA("id")).setAsString() = idStr;
 			cmd.Execute();
+			if (cmd.FetchNext())
+			{
+				return std::string(cmd.Field(1).asString().GetMultiByteChars());
+			}
+			return "";
 		}
 		catch (SAException& ex)
 		{
@@ -59,11 +66,11 @@ public:
 #endif // DEBUG
 			throw std::exception(ex.ErrText().GetMultiByteChars());
 		}
-		return nullptr;
+		return "";
 	}
 
-private:
-	template<typename Type, std::enable_if_t<std::is_class_v<Type>, bool> = true>
+public:
+	template<typename Type = T, std::enable_if_t<std::is_class_v<Type>, bool> = true>
 	int field_count()
 	{
 		int count = 0;
@@ -78,7 +85,7 @@ private:
 		});
 	}
 
-	template<typename Type, std::enable_if_t<std::is_void_v<Type>, bool> = true>
+	template<typename Type = T, std::enable_if_t<std::is_void_v<Type>, bool> = true>
 	auto field_count()
 	{
 		return 0;
@@ -86,49 +93,63 @@ private:
 
 
 
-	template <typename Type, std::enable_if_t<std::is_class_v<Type>, bool> = true>
-	std::string table_alias_builder( std::vector<std::string> table_list )
+	template <typename Type = T, std::enable_if_t<std::is_class_v<Type>, bool> = true>
+	std::string include_table( std::vector<std::string> table_list )
 	{
+		std::string table_name = std::string(typeid(Type).name());
+		table_name = table_name.substr(table_name.find_last_of(' ') + 1);
 		std::string alias = "";
 		Type t{};
 		boost::mp11::mp_for_each< boost::describe::describe_members<Type, boost::describe::mod_any_access | boost::describe::mod_inherited>>([&](auto D)
 		{
 			auto value = t.*(D).pointer;
-			if (is_primitive_type(value))
+			if (std::find(table_list.begin(), table_list.end(), D.name) != table_list.end())
 			{
-				alias += "[" + table_list.back() + "]" + "." + (D).name + " AS " + table_list.back() + "_" + (D).name + ", ";
-			}
-			else if (std::find(table_list.begin(), table_list.end(), D.name) != table_list.end())
-			{
-				using type = std::remove_reference_t<decltype(item.get()->*(D).pointer)>;
-				using inner_type = has_value_type_t<type>;
+				alias += ", (SELECT *";
+				using val_type = std::remove_reference_t<decltype(value)>;
+				using inner_type = has_value_type_t<val_type>;
+				
+				std::string inner_table_name;
 
-				if (!std::is_void_v<inner_type> && !std::is_same_v<std::string, type> && !std::is_same_v<std::wstring, type>)
+				table_list.erase(std::remove_if(table_list.begin(), table_list.end(), [&](std::string s) { return s == D.name; }), table_list.end());
+
+				if (!std::is_void_v<inner_type> && !std::is_same_v<std::string, val_type> && !std::is_same_v<std::wstring, val_type>)
 				{
 					using inner_elem_type = std::remove_pointer_t<has_element_type_t<std::remove_reference_t<inner_type>>>;
 
 					if (!std::is_void_v<inner_elem_type>)
 					{
-
+						inner_table_name = std::string(typeid(inner_elem_type).name());
+						inner_table_name = inner_table_name.substr(inner_table_name.find_last_of(' ') + 1);
+						alias += include_table<inner_elem_type>(table_list);
+						alias += " FROM [dbo].[" + inner_table_name + "] WHERE [dbo].[" + table_name + "].Id = [dbo].[" + inner_table_name + "]." + table_name + "Id FOR JSON AUTO) AS '" + inner_table_name + "'";
 					}
 				}
-				else if (is_shared_ptr_v<type>)
+				else if (is_shared_ptr_v<val_type>)
 				{
-					using inner_elem_type = std::remove_pointer_t<has_element_type_t<std::remove_reference_t<type>>>;
+					using inner_elem_type = std::remove_pointer_t<has_element_type_t<std::remove_reference_t<val_type>>>;
 					if (!std::is_void_v<inner_elem_type>)
 					{
-
+						inner_table_name = std::string(typeid(inner_elem_type).name());
+						inner_table_name = inner_table_name.substr(inner_table_name.find_last_of(' ') + 1);
+						alias += include_table<inner_elem_type>(table_list);
+						alias += " FROM [dbo].[" + inner_table_name + "] WHERE [dbo].[" + inner_table_name + "].Id = [dbo].[" + table_name + "]." + inner_table_name + "Id FOR JSON AUTO) AS '" + inner_table_name + "'";
 					}
 				}
 
 			}
 		});
+		return alias;
 	}
 
-	template <typename Type, std::enable_if_t<std::is_void_v<Type>, bool> = true>
-	auto table_alias_builder(std::vector<std::string> table_list)
+	template <typename Type = T, std::enable_if_t<std::is_void_v<Type>, bool> = true>
+	std::string include_table(std::vector<std::string> table_list)
 	{
-		return 0;
+		return "";
 	}
+
+
+	private:
+		std::unique_ptr<DbContext> db;
 
 };
--- Application/TodoItemService.cpp
+++ Application/TodoItemService.cpp
@@ -10,6 +10,7 @@ TodoItemService::TodoItemService()
 
 std::shared_ptr<TodoItem> TodoItemService::GetTodoItemById(const std::string& Id) noexcept(false)
 {
+
 	TodoItemQuery todo_item_query;
 	return todo_item_query.GetSingle(EQ(Id), { "TodoList" });
 }
